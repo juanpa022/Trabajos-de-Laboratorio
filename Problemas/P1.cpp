21103f7344cc6ed2a1c2d5ca7f3847f9f9e5dcd9
juanpa022 <54367742+juanpa022@users.noreply.github.com>
2026-03-23 14:33:36 -0500

    Refactor matrix multiplication with random initialization
    Replaced the original implementation with a new version that initializes a random matrix and performs matrix-vector multiplication, measuring execution time for both loop orders.

--- Problemas/P1.cpp
+++ Problemas/P1.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <chrono>
-using namespace std;
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+const int max = 100;
 
 int main() {
-    int MAX = 10000;
-    vector<vector<double>> A(MAX, vector<double>(MAX, 1.0));
-    vector<double> x(MAX, 1.0), y(MAX, 0.0);
+    double A[max][max], x[max], y[max];
 
-    auto t1 = chrono::high_resolution_clock::now();
-    for (int i = 0; i < MAX; i++)
-        for (int j = 0; j < MAX; j++)
-            y[i] += A[i][j] * x[j]; 
-    auto t2 = chrono::high_resolution_clock::now();
-    cout << "Rapido: " << chrono::duration<double>(t2 - t1).count() << "s" << endl;
-    for (int i = 0; i < MAX; i++) y[i] = 0; 
-    auto t3 = chrono::high_resolution_clock::now();
-    for (int j = 0; j < MAX; j++)
-        for (int i = 0; i < MAX; i++)
+    srand(time(0));
+    for (int i = 0; i < max; i++) {
+        x[i] = rand() % 100;
+        y[i] = 0;
+        for (int j = 0; j < max; j++) {
+            A[i][j] = rand() % 100;
+        }
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < max; i++)
+        for (int j = 0; j < max; j++)
             y[i] += A[i][j] * x[j];
-    auto t4 = chrono::high_resolution_clock::now();
-    cout << "Lento: " << chrono::duration<double>(t4 - t3).count() << "s" << endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Tiempo del primer par de bucles: " << duration.count() << " segundos" << std::endl;
+
+    for (int i = 0; i < max; i++) {
+        y[i] = 0;
+    }
+
+    start = std::chrono::high_resolution_clock::now();
+
+    for (int j = 0; j < max; j++)
+        for (int i = 0; i < max; i++)
+            y[i] += A[i][j] * x[j];
+
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Tiempo del segundo par de bucles: " << duration.count() << " segundos" << std::endl;
 
     return 0;
 }
